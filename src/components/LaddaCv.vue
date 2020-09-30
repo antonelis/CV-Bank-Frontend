76533732cdfebd4377a666fc53c9330c2553750f
Puttzon <puttzon@gmail.com>
2020-09-30 11:30:57 +0200

    ny testkod

--- src/components/LaddaCv.vue
+++ src/components/LaddaCv.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto">
-    <div class="form-container mx-auto">
+    <!-- <div class="form-container mx-auto">
       <h1>Ladda upp Cv</h1>
       <h6>Här kan du ladda upp ditt Cv till ITHS Cv-databas.</h6>
       <form>
@@ -15,13 +15,34 @@
           <button class="btn-color mt-5">Ladda upp</button>
         </div>
       </form>
-    </div>
+    </div> -->
+
+    <!-- TEST -->
+    <input type="file" @change="onFileChanged" />
+    <button @click="onUpload">Upload!</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "LaddaCv",
+
+  data() {
+    return {
+      selectedFile: null,
+    };
+  },
+  methods: {
+    onFileChanged(event) {
+      this.selectedFile = event.target.files[0];
+    },
+    onUpload() {
+      // upload file, get it from this.selectedFile
+      const formData = new FormData();
+      formData.append("myFile", this.selectedFile, this.selectedFile.name);
+      axios.post("http://localhost:8080/uploadFile", formData);
+    },
+  },
 };
 </script>
 
